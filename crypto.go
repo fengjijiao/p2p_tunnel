00c23af35ce72f26203b4a23d2a8f4c1afe59c98
fengjijiao <16521693+fengjijiao@users.noreply.github.com>
2020-02-12 04:32:17 -0500

    add strong crypto of tink

--- crypto.go
+++ crypto.go
@@ -1,61 +1,106 @@
 package main
 
 import (
-    "bytes"
-    "crypto/cipher"
-    "crypto/aes"
-    "encoding/base64"
     "fmt"
+    "log"
+    "github.com/golang/protobuf/proto"
+    "github.com/google/tink/go/aead"
+    "github.com/google/tink/go/keyset"
+    "github.com/google/tink/go/insecurecleartextkeyset"
+    "encoding/base64"
+    "bytes"
 )
 
-func PKCS7Padding(ciphertext []byte, blockSize int) []byte {
-    padding := blockSize - len(ciphertext) % blockSize
-    padtext := bytes.Repeat([]byte{byte(padding)}, padding)
-    return append(ciphertext, padtext...)
-}
+var kh *keyset.Handle
 
-func PKCS7UnPadding(origData []byte) []byte {
-    length := len(origData)
-    unpadding := int(origData[length-1])
-    return origData[:(length - unpadding)]
-}
+func test_crypto() {
+    kh, _ = keyset.NewHandle(aead.AES128GCMKeyTemplate())
+    // first test
+    a, err := aead.New(kh)
+    if err != nil {
+        log.Fatal(err)
+    }
 
-func AesEncrypt(origData, key []byte) ([]byte, error) {
-    block, err := aes.NewCipher(key)
+    ct, err := a.Encrypt([]byte("this data needs to be encrypted"), []byte("associated data"))
     if err != nil {
-        return nil, err
+        log.Fatal(err)
     }
-    blockSize := block.BlockSize()
-    origData = PKCS7Padding(origData, blockSize)
-    blockMode := cipher.NewCBCEncrypter(block, key[:blockSize])
-    crypted := make([]byte, len(origData))
-    blockMode.CryptBlocks(crypted, origData)
-    return crypted, nil
-}
 
-func AesDecrypt(crypted, key []byte) ([]byte, error) {
-    block, err := aes.NewCipher(key)
+    pt, err := a.Decrypt(ct, []byte("associated data"))
     if err != nil {
-        return nil, err
+        log.Fatal(err)
     }
-    blockSize := block.BlockSize()
-    blockMode := cipher.NewCBCDecrypter(block, key[:blockSize])
-    origData := make([]byte, len(crypted))
-    blockMode.CryptBlocks(origData, crypted)
-    origData = PKCS7UnPadding(origData)
-    return origData, nil
-}
 
-func test() {
-    key := []byte("0123456789abcdef")
-    result, err := AesEncrypt([]byte("hello world"), key)
-    if err != nil {
-        panic(err)
+    fmt.Printf("Cipher text: %s\nPlain text: %s\n", ct, pt)
+    // first test
+    exportedPriv := &keyset.MemReaderWriter{}
+    if err := insecurecleartextkeyset.Write(kh, exportedPriv); err != nil {
+            log.Fatalf("unexpected error writing keyset: %v", err)
     }
-    fmt.Println(base64.StdEncoding.EncodeToString(result))
-    origData, err := AesDecrypt(result, key)
+    ksPriv, _ := proto.Marshal(exportedPriv.Keyset)
+    bksPriv := base64.StdEncoding.EncodeToString(ksPriv)
+    createFile("priv.txt", []byte(bksPriv))
+    fmt.Println(bksPriv)
+    bksPub := readFile("priv.txt")
+    ksPub, _ := base64.StdEncoding.DecodeString(string(bksPub))
+    khPub, err := insecurecleartextkeyset.Read(keyset.NewBinaryReader(bytes.NewReader(ksPub)))
+    // second test
+    a, err = aead.New(khPub)
     if err != nil {
-        panic(err)
+        log.Fatal(err)
     }
-    fmt.Println(string(origData))
+
+    ct, err = a.Encrypt([]byte("this data needs to be encrypted"), []byte("associated data"))
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    pt, err = a.Decrypt(ct, []byte("associated data"))
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    fmt.Printf("Cipher text: %s\nPlain text: %s\n", ct, pt)
+    // second test
+}
+func patchKeysetHandle(filepath string) {
+    if(existsFile(filepath)){
+        bksPub := readFile(filepath)
+        ksPub, _ := base64.StdEncoding.DecodeString(string(bksPub))
+        kh, _ = insecurecleartextkeyset.Read(keyset.NewBinaryReader(bytes.NewReader(ksPub)))
+    }else{
+        kh, _ = keyset.NewHandle(aead.AES128GCMKeyTemplate())
+        exportedPriv := &keyset.MemReaderWriter{}
+        if err := insecurecleartextkeyset.Write(kh, exportedPriv); err != nil {
+            log.Fatalf("unexpected error writing keyset: %v", err)
+        }
+        ksPriv, _ := proto.Marshal(exportedPriv.Keyset)
+        bksPriv := base64.StdEncoding.EncodeToString(ksPriv)
+        createFile(filepath, []byte(bksPriv))
+    }
+}
+// cryptOfAEAD
+// AEAD加密&解密
+func cryptOfAEAD(text []byte, key []byte, isDecrypt bool) []byte {
+    //log.Printf("%+v\n", kh)
+    //log.Printf("原文：’%v‘\n", text)
+    aen, err := aead.New(kh)
+    if err != nil {
+        log.Fatal(err)
+    }
+    if(!isDecrypt){
+        ct, err := aen.Encrypt(text, key)
+        if err != nil {
+            log.Fatal(err)
+        }
+        //log.Printf("加密结果：’%v‘\n", ct)
+        return ct
+    }else{
+        pt, err := aen.Decrypt(text, key)
+        if err != nil {
+            log.Fatal(err)
+        }
+        //log.Printf("解密结果：‘%v’\n", pt)
+        return pt
+    }
 }
